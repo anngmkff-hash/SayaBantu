57f3530d993823d5ccd61801ed73c051f370d80b
anngmkff-hash <anang.makruf241206@gmail.com>
2025-12-09 08:24:38 +0700

    Update LoginPage.tsx

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,7 +1,7 @@
 // src/LoginPage.tsx
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom'; // <-- tambah Link
-import { IconContext } from 'react-icons';
+import { IconContext } from 'react-icons/fa';
 import { HiOutlineMail, HiOutlineLockClosed } from 'react-icons/hi';
 
 const LoginPage: React.FC = () => {
